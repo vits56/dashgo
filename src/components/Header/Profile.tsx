7af1775bf47541f76d5a245bc54e84f99ffebf17
vits56 <bastosduartebruno@gmail.com>
2022-01-16 06:04:46 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,22 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
   return (
     <Flex align='center'>
-    <Box>
-      <Text mr='4' textAlign='right'>
-        Bruno B Duarte
-      </Text>
-      <Text color='gray.300' fontSize='small'>
-        bastosduartebruno@gmail.com
-      </Text>
-    </Box>
+      { showProfileData && (
+          <Box>
+            <Text mr='4' textAlign='right'>
+              Bruno B Duarte
+            </Text>
+            <Text color='gray.300' fontSize='small'>
+              bastosduartebruno@gmail.com
+            </Text>
+          </Box>
+      )}
 
     <Avatar ml='4' size='md' name='Bruno Bastos' src='https://github.com/vits56.png' />
 
