a47120b4b107a3403cabac5f8117da4a3aa16545
vits56 <bastosduartebruno@gmail.com>
2022-01-16 08:07:23 -0300

    Listagem responsiva

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -6,6 +6,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Box>
       <Header />
@@ -31,17 +36,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de Cadastro</Th>
+                { isWideVersion && <Th>Data de Cadastro</Th> }
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                 <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -50,7 +55,7 @@ export default function UserList() {
                     <Text fontSize="sm" coloe="gray.300">bastosduartebruno@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>15 de janeiro de 2022</Td>
+                { isWideVersion && <Td>15 de janeiro de 2022</Td> }
                 <Td>
                 <Button
                   AS="a"
@@ -64,7 +69,7 @@ export default function UserList() {
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                 <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -73,7 +78,7 @@ export default function UserList() {
                     <Text fontSize="sm" coloe="gray.300">bastosduartebruno@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>15 de janeiro de 2022</Td>
+                { isWideVersion && <Td>15 de janeiro de 2022</Td>}
                 <Td>
                 <Button
                   AS="a"
@@ -87,7 +92,7 @@ export default function UserList() {
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                 <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -96,7 +101,7 @@ export default function UserList() {
                     <Text fontSize="sm" coloe="gray.300">bastosduartebruno@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>15 de janeiro de 2022</Td>
+                { isWideVersion && <Td>15 de janeiro de 2022</Td>}
                 <Td>
                 <Button
                   AS="a"
